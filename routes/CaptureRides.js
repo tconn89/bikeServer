d13bd13d571e39bab22e335399752fdbcc756cc4
Ty Connors <tconn89@gmail.com>
2018-10-28 12:14:47 -0400

    Refactor repo class

--- routes/CaptureRides.js
+++ routes/CaptureRides.js
@@ -3,97 +3,87 @@ const {db} = require('../firebase');
 const router = Express.Router();
 
 class Repo {
-  create(name, coords){
+  static async create(name, coords){
     var newRideKey = db.ref().child('rides').push().key;
     // TODO Add validation
     let updates = {}
     updates['/rides/' + newRideKey] = { name, coords: {'0': coords} };
     await db.ref().update(updates);
-    return {}
+    return {key:newRideKey}
   }
-  update(key, coordsArray){
+  static async update(key, coordsArray){
     let updates = {}
+    const preCount = await Repo.coordsCount(key)
+    console.log('PreCount', preCount)
     if(typeof coordsArray.length == 'number'){
       coordsArray.forEach((coord,i)=> {
-        updates['/rides/' + key + '/coords'] = { [i]: coord };
+        updates[`/rides/${key}/coords/${preCount+i}`] = coord;
       })
     }
     else
       return {error: 'coordinates must be an array', status: 400}
     await db.ref().update(updates);
-    return {}
+    const count = await Repo.coordsCount(key)
+    return {count}
   }
-  rawIndex(){
+  static async index(){
+    const rides = await Repo.rawIndex()
+    return Repo.convertToArray(rides)
+  }
+  static async read(key){
+    let snapshot = await db.ref(`rides/${key}`).once('value')
+      .catch(error => console.log(error));
+    return snapshot.val()
+  }
+  static async coordsCount(key){
+    let snapshot = await db.ref(`rides/${key}/coords`).once('value')
+      .catch(error => console.log(error));
+      const tmp = snapshot.val()
+      console.log('Snapshot', tmp)
+      return Object.keys(tmp).length
+  }
+  static async rawIndex(){
     let snapshot = await db.ref('rides').once('value')
       .catch(error => console.log(error));
     return snapshot.val()
   }
-  convertToArray(){
-    return Object.keys(rides).map( routeKey => {
+  static convertToArray(rides){
+    const map = Object.keys(rides).map( routeKey => {
       if(routeKey[0] == '-'){
         let coords = Object.keys(rides[routeKey]['coords']).map(coordKey => (rides[routeKey]['coords'][coordKey]) )
         return {name: rides[routeKey]['name'], coords, id: routeKey} 
       }
       return null
     })
+    return map.filter(route => route != null)
   }
 }
 
-router.get('/', async (req, res) => {
-  let t = await db.ref('rides').child(1).once('value')
-    .catch(error => console.log(error));
-  res.send({result: t})
+router.get('/read/:key', async (req, res) => {
+  console.log(req.params)
+  if(req.params.key == undefined)
+    return res.status(400).send({error: 'key must be defined'})
+  let result = await Repo.read(req.params.key)
+  res.send({result})
 })
 
 router.get('/index', async (req, res) => {
   console.log('Indexing Bike Routes')
-  let snapshot = await db.ref('rides').once('value')
-    .catch(error => console.log(error));
-  let rides = snapshot.val()
-
-  let result;
-  let errors = []
-  try {
-  result = Object.keys(rides).map( routeKey => {
-    if(routeKey[0] == '-'){
-      let coords = Object.keys(rides[routeKey]['coords']).map(coordKey => (rides[routeKey]['coords'][coordKey]) )
-      return {name: rides[routeKey]['name'], coords, id: routeKey} 
-    }
-    return null
-  })
-  result = result.filter(route => route != null)
-} catch(error){console.log(error); errors.push(error)}
-  
-  res.send({result, errors})
+  const result = await Repo.index()
+  res.send({result})
 })
 
 router.post('/create', async (req, res) => {
-  console.log(req.body)
   const {name, coords } = req.body.route
   console.log('create ' + name)
-  var newRideKey = db.ref().child('rides').push().key;
-  // TODO Add validation
-  let updates = {}
-  updates['/rides/' + newRideKey] = { name, coords: [] };
-  await db.ref().update(updates);
-  await db.ref(`rides/${newRideKey}`).child('coords').push(coords)
-  res.send({key: newRideKey})
+  const {key} = await Repo.create(name, coords)
+  res.send({key})
 })
 router.post('/', async (req, res) => {
   const { key, ride } = req.body
   console.log(`Updating ${key}`)
-
-  if(typeof ride.length == 'number')
-    ride.forEach(async location =>
-      await db.ref(`rides/${key}`).child('coords').push(location)
-        .catch(error => console.log(error))
-    );
-  else
-    await db.ref(`rides/${key}`).child('coords').push(ride)
-      .catch(error => console.log(error));
-  let coords = await db.ref(`rides/${key}`).once('value')
-  let val = await coords.val()
-  res.send({count: Object.keys(val.coords).length})
+  const {count, error} = await Repo.update(key, ride)
+  res.send({count, error})
 })
 
 module.exports = {router };
--- test/curl/ongoing_ride.sh
+++ test/curl/ongoing_ride.sh
@@ -1,4 +1,4 @@
 curl -H "Content-Type: application/json" \
      -X POST \
-     -d '{"key": "-LPdAPpkCcqXM6MmteqN", "ride": {"time": 1540435065559, "lat": 35.779284, "long": -78.650596}}' \
+     -d '{"key": "-LPr0OCY3zpu38Vk-eRj", "ride": [{"time": 1540435065559, "lat": 35.779284, "lng": -78.650596}]}' \
      http://localhost:4000/CaptureRides
